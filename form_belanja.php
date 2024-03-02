89963ff0a9712e1584f30c5d797f2a77be3775f6
wwwaaawwwaaa3 <146153700+wwwaaawwwaaa3@users.noreply.github.com>
2024-03-02 20:39:24 +0700

    Update form_belanja.php

--- form_belanja.php
+++ form_belanja.php
@@ -1,15 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $Nama_customer = $_POST['text']; // Perbaikan nama variabel
-    $Beli_produk = $_POST['radio']; // Perbaikan nama variabel
-    $Jumlah_beli = $_POST['text1']; // Perbaikan nama variabel
+    $Nama_customer = $_POST['text']; 
+    $Beli_produk = $_POST['radio']; 
+    $Jumlah_beli = $_POST['text1']; 
 }
 
 if ($Beli_produk == 'Televisi') $harga = 4200000;
 else if ($Beli_produk == 'Kulkas') $harga = 3100000;
-else if ($Beli_produk == 'Mesin cuci') $harga = 3800000; // Perbaikan nilai 'Mesin cuci'
+else if ($Beli_produk == 'Mesin cuci') $harga = 3800000; 
 
-$harga_total = $Jumlah_beli * $harga; // Menggunakan nama variabel yang benar
+$harga_total = $Jumlah_beli * $harga; 
 
 echo '</br>Nama customer : '. $Nama_customer;
 echo '</br>Beli produk : '. $Beli_produk;
